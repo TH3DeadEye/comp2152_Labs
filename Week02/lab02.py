5ea14b23bcd8745f8051ab8de1083d73cd2ad92b
Arman <armanmilani84@gmail.com>
2026-01-25 20:55:40 -0500

    week02

---
+++ Week02/lab02.py
@@ -0,0 +1,23 @@
+import random
+
+choices = ["Rock", "Paper", "scissors"]
+
+playerChoice= input("Enter the number 1 to 3 for the folloing choices 1-Rock, 2-paper, 3-scisseors: ")
+
+playerChoice = int(playerChoice)
+
+if playerChoice not in [1, 2, 3]:
+    print("Error: Choice shoud be between 1 and 3!")
+else: 
+    computerChoice = random.randint(1, 3)
+
+    if playerChoice == computerChoice:
+        print("It's tie!")
+    elif playerChoice == 1 and computerChoice == 3:
+        print("Rock beats Scissors -- You win!")
+    elif playerChoice == 2 and computerChoice == 1:
+        print("Paper beats Rock -- You win!")
+    elif playerChoice == 3 and computerChoice == 2:
+        print("Scissors beats Paper -- You win!")
+    else:
+        print("You lose!")
